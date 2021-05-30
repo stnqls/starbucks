19950e7fe6e40fdf090a896e7968d2a41ea2ace3
stnqls <stnqls3938@gmail.com>
2021-05-31 01:55:11 +0900

    Image slide and slide toggle

--- js/main.js
+++ js/main.js
@@ -45,9 +45,26 @@ fadeEls.forEach(function (fadeEl,index){
   });
 });
 
-
+//new Swiper(선택자, 옵션)
 new Swiper('.notice-line .swiper-container', {
   direction : 'vertical',
   autoplay : true,
   loop : true
+});
+new Swiper('.promotion .swiper-container', {
+  slidesPerView: 3, //한번에 보여줄 슬라이드 개수
+  spaceBetween : 10, //슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 오도록
+  loop : true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination : {
+    el : '.promotion .swiper-pagination', //페이지 번호 요소 선택자
+    clickable : true // 사용자의 페이지 번호 요소 제어
+  },
+  navigation:{
+    preveEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
 });
